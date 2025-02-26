10a733d427d10d05811724d07f4bed22680ef1ff
simoncheam <simon@simoncheam.dev>
2025-02-25 21:51:29 -0500

    debug

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   env: {
     AWS_REGION: process.env.AWS_REGION,
     LAMBDA_FUNCTION_ARN: process.env.LAMBDA_FUNCTION_ARN,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
 
   },
 };
